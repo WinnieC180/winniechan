07267ea8586648e1a47d874ba3c02dd4eb6dcce4
Winnie Chan <wchan0806@gmail.com>
2025-12-31 20:58:02 -0500

    Made the header responsive to mobile sizes
    - Added a toggle icon when the screen is a certain size (didn't create the menu for the toggle yet)
    - Made the header and logo change depending on the size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./style.css";
 import winLogo from "../assets/logo.svg";
+import {X, Menu} from "lucide-react"
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const links = [
     { name: "Work", href: "#work" },
     { name: "About", path: "/about" },
@@ -13,7 +17,9 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/"><img src={winLogo} alt="Winnie's Logo, Chinese character for win" /></Link>
+        <Link to="/">
+          <img src={winLogo} alt="Winnie's Logo, Chinese character for win" />
+        </Link>
       </div>
       <div className="links">
         <ul>
@@ -28,6 +34,7 @@ function NavBar() {
           ))}
         </ul>
       </div>
+      <button onClick = {(() => setIsMenuOpen((prev) => !prev))} className="menu">{isMenuOpen ? <X size={24}/> : <Menu size={24}/>}</button>
     </nav>
   );
 }
