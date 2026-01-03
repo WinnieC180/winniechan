fe5b238db0606df5289813fb86af5a28cf5d7be3
Winnie Chan <wchan0806@gmail.com>
2026-01-03 14:36:55 -0500

    Made the texts responsive in the work page

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,15 +13,15 @@ function Works() {
   return (
     <section className="centerFlex" style={{...direction, ...gap}} id="work" >
       <div className="centerFlex">
-        <img src={Line} alt="A short line" />
-        <p>EXPEDITION LOG</p>
-        <img src={Line} alt="A short line" />
+        <img src={Line} alt="A short line" style={{width: "28vw"}}/>
+        <p style={{fontSize: "clamp(12px, 1.5vw + 0.5rem, 20px)"}}>EXPEDITION LOG</p>
+        <img src={Line} alt="A short line" style={{width: "28vw"}}/>
       </div>
       <div className="centerFlex" style={{ marginTop: "20px", ...direction, gap: "14px"}}>
-        <h1 className="exploring">
+        <h1 style={{fontSize: "clamp(2rem, 3vw + 1rem, 5rem)"}}>
           Exploring <span style={{ color: "#4ED3FF" }}>My Work</span>
         </h1>
-        <p style={{textAlign: "center", width: "40%"}}>
+        <p style={{textAlign: "center", width: "min(580px, 80%)", fontSize: "clamp(12px, 1.5vw + 0.5rem, 20px)"}}>
           Each project represents a voyage into the unknown waters where I
           discover new challenges and skills along the way ~
         </p>
