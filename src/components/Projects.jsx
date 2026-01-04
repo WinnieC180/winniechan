e0d592e136726ce039e6fb744afb3d98b540dbf9
Winnie Chan <wchan0806@gmail.com>
2026-01-04 01:22:11 -0500

    Formatted the projects file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import "./projects.css"
+import "./projects.css";
 
 function Projects() {
   const projects = [
@@ -9,7 +9,7 @@ function Projects() {
         "A wholesome pet sim… with a very lumpy twist! A fun game developed and designed for the CS3110 final project.",
       timeSpent: "3 weeks",
       tools: ["OCaml", "Raylib"],
-      isComplete: true
+      isComplete: true,
     },
     {
       title: "A mole, a goose, or...?",
@@ -18,7 +18,7 @@ function Projects() {
         "A whack a mole game but without a mole?? drew and implemented a interactive project for APCSA.",
       timeSpent: "1 month",
       tools: ["Java", "Medibang Paint"],
-      isComplete: true
+      isComplete: true,
     },
     {
       title: "A Friend to Lock in with",
@@ -27,7 +27,7 @@ function Projects() {
         "Won best backend award in AppDev’s Hack Challenge. An app that helps Cornell students study friends!",
       timeSpent: "1 week",
       tools: ["Figma", "Swift", "Docker"],
-      isComplete: true
+      isComplete: true,
     },
     {
       title: "YouTube Case Study",
@@ -36,7 +36,7 @@ function Projects() {
         "Conducted user research to design a new feature of YouTube to help users create more personalized recommendations.",
       timeSpent: "3 months",
       tools: ["Figma"],
-      isComplete: true
+      isComplete: true,
     },
     {
       title: "Sustainabytes",
@@ -45,45 +45,48 @@ function Projects() {
         "Developing the website of Sustainabytes @ Cornell University. Spreading Computational Sustainability.",
       timeSpent: "......",
       tools: ["Figma", "React", "CSS", "JavaScript"],
-      isComplete: false
+      isComplete: false,
     },
   ];
 
   return (
     <div className="projects">
-        {projects.map((project) => (
-            <div className={project.isComplete ? "project" : "project notDone"}>
-                {project.isComplete ? 
-                    (<div className="header" style={{color: "var(--clr-secondary-300)"}}>
-                        <p>MISSON COMPLETE</p>
-                        <p>~ {project.timeSpent}</p>
-                    </div>) : 
-                    (<div className="header" style={{color: "hsl(352 100% 65%)"}}>
-                        <p>CURRENTLY EXPLORING</p>
-                        <p>......</p>
-                    </div>
-                    )
-                }
-                
-                <div className="image">
-
-                </div>
-
-                <div className="name" style={{display: "flex"}}>
-                    <h2 className="title">{project.title} <span>✦ {project.subTitle}</span></h2>
-                </div>
-                
-                <p className="projectBio" style={{color: "var(--clr-primary-500)"}}>
-                    {project.description}
-                </p>
-
-                <div className="tools">
-                    {project.tools.map ((tool) => (
-                        <p className="tool">{tool}</p>
-                    ))}
-                </div>
+      {projects.map((project) => (
+        <div className={project.isComplete ? "project" : "project notDone"}>
+          {project.isComplete ? (
+            <div
+              className="header"
+              style={{ color: "var(--clr-secondary-300)" }}
+            >
+              <p>MISSON COMPLETE</p>
+              <p>~ {project.timeSpent}</p>
             </div>
-        ))}
+          ) : (
+            <div className="header" style={{ color: "hsl(352 100% 65%)" }}>
+              <p>CURRENTLY EXPLORING</p>
+              <p>......</p>
+            </div>
+          )}
+
+          <div className="image"></div>
+
+          <div className="name" style={{ display: "flex" }}>
+            <h2 className="title">
+              {project.title} <span>✦ {project.subTitle}</span>
+            </h2>
+          </div>
+
+          <p className="projectBio" style={{ color: "var(--clr-primary-500)" }}>
+            {project.description}
+          </p>
+
+          <div className="tools">
+            {project.tools.map((tool) => (
+              <p className="tool">{tool}</p>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
