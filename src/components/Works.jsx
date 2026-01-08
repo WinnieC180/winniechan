91554b26dd1a88aff34571bc18c8d11fc65d5d16
Winnie Chan <wchan0806@gmail.com>
2026-01-08 01:49:23 -0500

    Fixed the title and the favicon issue

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,6 @@
 import Line from "../assets/line.png";
 import Projects from "./Projects";
+import { useEffect } from "react";
 
 function Works() {
   const gap = {
@@ -10,18 +11,37 @@ function Works() {
     flexDirection: "column",
   };
 
+  useEffect(() => {
+    document.title = "Winnie Chan";
+  }, []);
+
   return (
-    <section className="centerFlex" style={{...direction, ...gap, scrollMarginTop: "120px"}} id="work" >
+    <section
+      className="centerFlex"
+      style={{ ...direction, ...gap, scrollMarginTop: "120px" }}
+      id="work"
+    >
       <div className="centerFlex">
-        <img src={Line} alt="A short line" style={{width: "28vw"}}/>
-        <p style={{fontSize: "clamp(12px, 1.5vw + 0.5rem, 20px)"}}>EXPEDITION LOG</p>
-        <img src={Line} alt="A short line" style={{width: "28vw"}}/>
+        <img src={Line} alt="A short line" style={{ width: "28vw" }} />
+        <p style={{ fontSize: "clamp(12px, 1.5vw + 0.5rem, 20px)" }}>
+          EXPEDITION LOG
+        </p>
+        <img src={Line} alt="A short line" style={{ width: "28vw" }} />
       </div>
-      <div className="centerFlex" style={{ marginTop: "20px", ...direction, gap: "14px"}}>
-        <h1 style={{fontSize: "clamp(2rem, 3vw + 1rem, 5rem)"}}>
+      <div
+        className="centerFlex"
+        style={{ marginTop: "20px", ...direction, gap: "14px" }}
+      >
+        <h1 style={{ fontSize: "clamp(2rem, 3vw + 1rem, 5rem)" }}>
           Exploring <span style={{ color: "#4ED3FF" }}>My Work</span>
         </h1>
-        <p style={{textAlign: "center", width: "min(580px, 80%)", fontSize: "clamp(12px, 1.5vw + 0.5rem, 20px)"}}>
+        <p
+          style={{
+            textAlign: "center",
+            width: "min(580px, 80%)",
+            fontSize: "clamp(12px, 1.5vw + 0.5rem, 20px)",
+          }}
+        >
           Each project represents a voyage into the unknown waters where I
           discover new challenges and skills along the way ~
         </p>
